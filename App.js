4220309980edd65fe04d7930b4518e287a3dc455
ShaikhArmaan27 <72267363+ShaikhArmaan27@users.noreply.github.com>
2021-06-25 21:36:23 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
-import TransactionScreen from "./screens/BookTransactionScreen";
-import SearchScreen from "./screens/SearchScreen";
+import TransactionScreen from "./BookTransactionScreen";
+import SearchScreen from "./SearchScreen";
 
 export default class App extends React.Component {
   render() {
